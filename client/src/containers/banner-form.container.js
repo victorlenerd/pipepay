5df8a7f492660c461dfe5d92c0e73c43e9d1a2d0
sleekvick <nvonweb@outlook.com>
2018-10-19 14:07:19 +0100

    Update pre-commit hooks

--- client/src/containers/banner-form.container.js
+++ client/src/containers/banner-form.container.js
@@ -2,20 +2,21 @@
 import React from "react";
 import Header from "components/header";
 
-type Props = { title: string, children?: React.Node  };
+type Props = { title: string, children?: React.Node };
 
 const BannerForm = ({ title, children }: Props) => (
 	<React.Fragment>
 		<Header />
 		<div className="col-lg-6 col-md-6 col-sm-12 col-xs-1 cloths-bg" id="noPad">
-			<div className="overlay"></div>
+			<div className="overlay" />
 		</div>
 		<div className="col-lg-6 col-md-6 col-sm-12 col-xs-12 left-from-content">
 			<div className="container-main">
 				<div className="header">
 					<h1>{title}.</h1>
 				</div>
-				<br /><br />
+				<br />
+				<br />
 				{children}
 			</div>
 		</div>
