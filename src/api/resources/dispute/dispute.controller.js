9d3e6b6a9d34b0af84d6d4304aec39979c9fb909
sleekvick <nvonweb@outlook.com>
2018-11-18 03:17:49 +0100

    Don't keep records for disputes

--- src/api/resources/dispute/dispute.controller.js
+++ src/api/resources/dispute/dispute.controller.js
@@ -34,57 +34,45 @@ const DisputeController = generateController(DisputeModel, {
 		body.invoiceId = _id;
 
 		if (status === "paid") {
-			DisputeModel.create(body, async (err, doc) => {
-				if (err) {
-					return res.status(400).send({
-						error: {
-							message: "Could not create the dispute"
-						},
-						success: false
-					});
+			InvoiceModel.findOneAndUpdate(
+				{
+					_id
+				},
+				{
+					disputed: true,
+					status: "rejected"
+				},
+				async (err, doc) => {
+					if (err)
+						return res.status(400).send({
+							success: false,
+							error: err
+						});
+					try {
+						await sendDisputeMail(
+							marchantEmail,
+							customerEmail,
+							customerName,
+							marchantName,
+							body.reason,
+							body.from
+						);
+
+						res.send({
+							data: doc,
+							success: true
+						});
+					} catch (err) {
+						console.log("err", err);
+						return res.status(400).send({
+							error: {
+								message: "Could not send mail"
+							},
+							success: false
+						});
+					}
 				}
-
-				try {
-					InvoiceModel.findOneAndUpdate(
-						{
-							_id
-						},
-						{
-							disputed: true,
-							status: "rejected"
-						},
-						async (err, doc) => {
-							if (err)
-								return res.status(400).send({
-									success: false,
-									error: err
-								});
-
-							await sendDisputeMail(
-								marchantEmail,
-								customerEmail,
-								customerName,
-								marchantName,
-								body.reason,
-								body.from
-							);
-
-							res.send({
-								data: doc,
-								success: true
-							});
-						}
-					);
-				} catch (err) {
-					console.log("err", err);
-					return res.status(400).send({
-						error: {
-							message: "Could not send mail"
-						},
-						success: false
-					});
-				}
-			});
+			);
 		}
 	}
 });
