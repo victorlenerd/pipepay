1007a8f76d325b980032958fb3951e9145b104a3
Nwaokocha Victor <nvonweb@outlook.com>
2019-11-22 04:20:11 -0600

    keys.txt

--- src/api/modules/mailer.js
+++ src/api/modules/mailer.js
@@ -1,25 +1,34 @@
 import nodemailer from "nodemailer";
+import fs from "fs";
+import path from "path";
 const Sentry = require("@sentry/node");
 
-const key = require('./keys.json');
+let transporter = null;
 
-const transporter = nodemailer.createTransport({
-	host: "smtp.gmail.com",
-	port: 465,
-	secure: true,
-	auth: {
-		type: "OAuth2",
-		user: "hello@pipepay.co",
-		serviceClient: key.client_id,
-		privateKey: key.private_key
+fs.readFile(path.join(__dirname, "keys.txt"), "utf8", (err, data) => {
+	if (err) {
+		throw err;
 	}
+
+	const keys = JSON.parse(data);
+
+	transporter = nodemailer.createTransport({
+		host: "smtp.gmail.com",
+		port: 465,
+		secure: true,
+		auth: {
+			type: "OAuth2",
+			user: "hello@pipepay.co",
+			serviceClient: keys.client_id,
+			privateKey: keys.private_key
+		}
+	});
+
+	transporter.verify();
 });
 
-const from = "PipePay Team <hello@pipepay.co>";
-
 export const sendTo = mailOption => {
-	transporter.verify();
-	transporter.sendMail({ ...mailOption, from }, (error, info) => {
+	transporter.sendMail({ ...mailOption, from: "PipePay Team <hello@pipepay.co>" }, (error, info) => {
 		if (error) {
 			return Sentry.captureException(error);
 		}
